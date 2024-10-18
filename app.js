a3db2e8194223edb80c6f08925cad596c4e4f6f2
sachin <sk800968@gmail.com>
2024-10-18 06:18:13 +0530

    setup mongoose and dotenv

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(PORT, () => {
+      console.log(`Server is successfully listening on port ${PORT}...`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Dev");
+});
+
