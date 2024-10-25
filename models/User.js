28d0b0af07f74a9824bf930923e98572a71d90c6
sachin <sk800968@gmail.com>
2024-10-25 10:37:03 +0530

    added schema validation

--- models/User.js
+++ models/User.js
@@ -4,47 +4,64 @@ const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
-      minLength: 4,
-      maxLength: 50,
+      required: [true, "First name is required"],
+      minLength: [4, "First name must be at least 4 characters"],
+      maxLength: [50, "First name cannot exceed 50 characters"],
     },
 
     lastName: {
       type: String,
+      maxLength: [50, "Last name cannot exceed 50 characters"],
+      trim: true,
     },
 
     emailId: {
       type: String,
-      lowercase: true,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
+      lowercase: true,
       trim: true,
+      validate: {
+        validator: validator.isEmail,
+        message: (props) => `${props.value} is not a valid email address`,
+      },
     },
 
     password: {
       type: String,
-      required: true,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters long"],
+      validate: {
+        validator: validator.isStrongPassword,
+        message:
+          "Password must be stronger with a mix of letters, numbers, and symbols",
+      },
     },
+
     age: {
       type: Number,
-      min: 18,
+      min: [18, "Age must be at least 18"],
     },
 
     gender: {
       type: String,
       enum: { values: ["male", "female", "other"] },
     },
+
     photoUrl: {
       type: String,
       default: "https://geographyandyou.com/images/user-profile.png",
     },
+
     about: {
       type: String,
       default: "This is a default about of the user!",
     },
+
     skills: {
       type: [String],
     },
+    
   },
   {
     timestamps: true,
