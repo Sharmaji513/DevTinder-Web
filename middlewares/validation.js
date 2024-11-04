c32e24a66b0c212baa55d7733c217632f3ed6d40
sachin <sk800968@gmail.com>
2024-11-04 12:16:42 +0530

    added validation and encryption in auth.js

---
+++ middlewares/validation.js
@@ -0,0 +1,21 @@
+
+const validator = require("validator");
+
+const validateSignUpData = (req, res, next) => {
+  const { firstName, lastName, emailId, password } = req.body;
+
+  if (!firstName || !lastName) {
+    return res.status(400).json({ message: "Name is not valid!" });
+  } else if (!validator.isEmail(emailId)) {
+    return res.status(400).json({ message: "Email is not valid!" });
+  } else if (!validator.isStrongPassword(password)) {
+    return res.status(400).json({ message: "Please enter a strong password!" });
+  }
+  
+  // Proceed to the next middleware or route handler if validation is successful
+  next();
+};
+
+module.exports = {
+  validateSignUpData,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,38 +2,53 @@ const express = require("express");
 const User = require("../models/User");
 
 const authRouter = express.Router();
+const { validateSignUpData } = require("../middlewares/validation");
+const bcrypt = require("bcrypt");
+
 
 //Signup Route
-authRouter.post("/user/signup", async (req, res) => {
+authRouter.post("/user/signup", validateSignUpData, async (req, res) => {
   try {
     const { firstName, lastName, emailId, password } = req.body;
-    const user = new User({ firstName, lastName, emailId, password });
+
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    // console.log(passwordHash);
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
 
     res.status(201).send({ message: "User registered successfully" });
   } catch (error) {
-    res.status(400).send({ message: error.message });
+    res.status(500).send({ message: error.message });
   }
 });
+
 
 //login Route
 authRouter.post("/user/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
 
-    const user = await User.findOne({ emailId: emailId, password: password });
+    const user = await User.findOne({ emailId: emailId });
 
     if (!user) {
       throw new Error("Invalid credentials");
     }
 
-    res.status(201).send({ message: "User login successfully" });
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).send({ message: "Invalid credentials" });
+    res.status(201).send({ message:`${user.firstName} login successful` });
   } catch (error) {
     res.status(400).send({ message: error.message });
   }
 });
-
-
-
 
 module.exports = authRouter;
