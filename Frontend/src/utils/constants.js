b5ac0db5775acf9e7d7f47fa4ebe84090051dbc8
Sachin Kumar <66481063+Sharmaji513@users.noreply.github.com>
2024-12-08 12:32:27 +0530

    Update constants.js

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:7777/api/v1/"
+// export const BASE_URL = "http://localhost:7777/api/v1/"
+//Render 
+export const BASE_URL = "https://devtinder-web-backend.onrender.com"  
